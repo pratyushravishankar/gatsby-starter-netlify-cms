e73ec5ac8b9ae362acdfa4b0c70b25c08840012e
Pratyush Ravishankar <pratyushravishankar@gmail.com>
2020-06-11 13:42:18 +0100

    index - removed blog link

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -100,9 +100,9 @@ export const IndexPageTemplate = ({
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
+                    {/* <Link className="btn" to="/blog"> */}
+                      {/* Read more */}
+                    {/* </Link> */}
                   </div>
                 </div>
               </div>
